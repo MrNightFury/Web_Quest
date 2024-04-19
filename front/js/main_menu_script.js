50c9760f6138df2bfa6eb740290476f0413c10de
sergkhr <serg.khrechko@gmail.com>
2024-04-19 21:22:05 +0300

    main menu and inventory

---
+++ front/js/main_menu_script.js
@@ -0,0 +1,20 @@
+$("#mm_item_game_start").click(function() {
+    $("body > section").addClass("hidden");
+    $("#game").removeClass("hidden");
+    $("#inventory").removeClass("hidden");
+});
+
+$("#mm_item_settings").click(function() {
+    $(".mm_container").addClass("hidden");
+    $("#mm_settings").removeClass("hidden");
+});
+
+$("#mm_item_authors").click(function() {
+    $(".mm_container").addClass("hidden");
+    $("#mm_authors").removeClass("hidden");
+});
+
+$(".mm_back").click(function() {
+    $(".mm_container").addClass("hidden");
+    $("#mm_main").removeClass("hidden");
+});
