dba2502ddf0ce9f32cd0ba4ae81a332b2d000032
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2024-05-12 17:19:59 +0300

    Update main.ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,7 @@
 import { Controller } from "./engine/Controller.js"
 
-let packName = "Escape From Darkness"
+let urlParams = new URLSearchParams(window.location.search);
+let packName = urlParams.get("pack") ?? "Escape From Darkness";
+console.log(packName)
 var controller = Controller.instance;
 controller.setPack(packName);
