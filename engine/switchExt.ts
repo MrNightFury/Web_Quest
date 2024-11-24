98a938cb841c067885a77f832876db00c6aab1aa
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2024-11-24 12:44:55 +0300

    Some shinenigans for building for browser

--- engine/switchExt.ts
+++ engine/switchExt.ts
@@ -1,5 +1,4 @@
 import * as path from "https://deno.land/std@0.224.0/path/mod.ts";
-import { parse } from "https://cdn.skypack.dev/jsonc-parser";
 
 const rawArg = Deno.args[0];
 if (rawArg !== "ts" && rawArg !== "js") {
@@ -15,26 +14,25 @@ const d = {
 
 console.log("Switching imports to ", arg);
 
-const distDir = path.join(Deno.cwd(), 'js');
+const distDir = path.join(Deno.cwd(), 'src');
 
 processDir(distDir)
 
 async function processDir(dir: string) {
     const files = Deno.readDirSync(dir);
     for (const file of files) {
-        console.log(file.name);
+        // console.log(file.name);
 
         const filePath = path.join(dir, file.name);
         if (file.isDirectory) {
             processDir(filePath);
         }
-        if (file.isFile && filePath.endsWith('.js')) {
+        if (file.isFile && (filePath.endsWith('.js') || filePath.endsWith('.ts'))) {
             let content = await Deno.readTextFile(filePath);
 
-            // console.log(new RegExp(`(import .* from ['"].*)\.${d[arg]}(['"])`, "g"))
             content = content.replaceAll(new RegExp(`(import .* from ['"].*)\.${d[arg]}(['"])`, "g"), `$1.${arg}$2`);
             content = content.replaceAll(new RegExp(`(import ['"].*)\.${d[arg]}(['"])`, "g"), `$1.${arg}$2`);
-            content = content.replaceAll(new RegExp(`(import\(\p*['"].*).${d[arg]}(['"]\p*\))`, "g"), `$1.${arg}$2`);
+            content = content.replaceAll(new RegExp(`(import\p*\\(.*\.)${d[arg]}(['"]\\))`, "g"), `$1${arg}$2`);
 
             await Deno.writeTextFile(filePath, content);
         }
